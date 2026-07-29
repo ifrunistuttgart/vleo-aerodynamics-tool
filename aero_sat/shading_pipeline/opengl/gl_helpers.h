4c2361783bce55d36799749d4d6e4c35475b9865
Fabian Geyer <fabian.geyer@ifr.uni-stuttgart.de>
2026-07-29 14:09:11 +0200

    fix(shading_pipeline): use raise(SIGTRAP) instead of __debugbreak() on non-MSVC compilers
    __debugbreak() is an MSVC-only intrinsic; GCC/Clang don't support it, breaking Linux builds.

--- aero_sat/shading_pipeline/opengl/gl_helpers.h
+++ aero_sat/shading_pipeline/opengl/gl_helpers.h
@@ -2,7 +2,14 @@
 
 #include <GL/glew.h>
 
-#define ASSERT(x) if (!(x)) __debugbreak();
+#if defined(_MSC_VER)
+#define DEBUG_BREAK() __debugbreak()
+#else
+#include <csignal>
+#define DEBUG_BREAK() raise(SIGTRAP)
+#endif
+
+#define ASSERT(x) if (!(x)) DEBUG_BREAK();
 #define GLCall(x) GLClearError();\
     x;\
     ASSERT(GLLogCall(#x, __FILE__,__LINE__))
