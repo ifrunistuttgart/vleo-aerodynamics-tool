b7c349a249754de7c245c796efec91c1788169b4
Jan-Lukas Schröder <st177404@stud.uni-stuttgart.de>
2026-08-19 13:11:16 +0200

    tiny improvements/bugfixes

--- aero_sat/shading_pipeline/opengl/gl_helpers.h
+++ aero_sat/shading_pipeline/opengl/gl_helpers.h
@@ -3,10 +3,20 @@
 #include <GL/glew.h>
 
 #if defined(_MSC_VER)
-#define DEBUG_BREAK() __debugbreak()
+    // Microsoft Visual C++
+    #define DEBUG_BREAK() __debugbreak()
+#elif defined(__GNUC__) || defined(__clang__)
+    #if defined(_WIN32) || defined(_WIN64)
+        // GCC or Clang on Windows (MinGW / Strawberry Perl C++)
+        #define DEBUG_BREAK() __builtin_trap()
+    #else
+        // GCC or Clang on Linux / macOS
+        #include <csignal>
+        #define DEBUG_BREAK() raise(SIGTRAP)
+    #endif
 #else
-#include <csignal>
-#define DEBUG_BREAK() raise(SIGTRAP)
+    #include <csignal>
+    #define DEBUG_BREAK() raise(SIGABRT)
 #endif
 
 #define ASSERT(x) if (!(x)) DEBUG_BREAK();
