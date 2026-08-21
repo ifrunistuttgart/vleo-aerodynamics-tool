6cb18633a3e4624da0f113f33319f2b5c273e07f
Jan-Lukas Schröder <st177404@stud.uni-stuttgart.de>
2026-08-21 18:34:12 +0200

    Update matlab/custom_spdlog_sink.h
    Co-authored-by: Fabian Geyer <51081556+Fabian-Geyer@users.noreply.github.com>

--- matlab/custom_spdlog_sink.h
+++ matlab/custom_spdlog_sink.h
@@ -33,7 +33,7 @@ protected:
         spdlog::level::level_enum level = msg.level;
         std::string filename_clean = std::filesystem::path(msg.source.filename).filename().string();
         int matlab_level = map_spdlog_level(level);
-        matlab_logger_.log(maltab_level, s, filename_clean, msg.source.line);
+        matlab_logger_.log(matlab_level, s, filename_clean, msg.source.line);
     }
     void flush_() override {}
 
