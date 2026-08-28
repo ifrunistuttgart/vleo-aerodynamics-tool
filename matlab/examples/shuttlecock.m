18f3c37e29c3e00a77acd8e1893cf99f3c7301fc
Fabian Geyer <fabian.geyer@ifr.uni-stuttgart.de>
2026-08-28 17:20:10 +0900

    extend example with new visualizations

--- matlab/examples/shuttlecock.m
+++ matlab/examples/shuttlecock.m
@@ -23,11 +23,17 @@ fp.obj_file        = fullfile(fp.current_folder, ...
     "geometries/shuttlecock_15k.obj");
 
 satellite.geometry = vat.RotatableMeshGeometry(fp.obj_file);
+% satellite.geometry = vat.StaticMeshGeometry(fp.obj_file);
 satellite.verts    = satellite.geometry.get_vertices;
 
 n_tri = satellite.geometry.get_num_triangles;
 
 satellite.geometry.turn_mesh_around_axis(2, deg2rad(20), [0;0;0], [1;0;0])
 
-vat.show_mesh(satellite.geometry, zeros(n_tri,1), [1;0;0])
 
+vat.show_meshes(satellite.geometry)
+
+hinges(1).mesh_id = 4;
+hinges(1).origin = [-0.15; 0; -0.05]
+hinges(1).axis = [0;-1;0]
+vat.show_hinges(satellite.geometry, hinges)
